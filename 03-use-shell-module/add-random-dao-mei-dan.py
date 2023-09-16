517852482dedc2e7518ebe4647643e938d43b349
Kunqi Zhao <110202952+ZhaoKunqi@users.noreply.github.com>
2023-09-16 23:42:21 +0800

    Update add-random-dao-mei-dan.py

--- 03-use-shell-module/add-random-dao-mei-dan.py
+++ 03-use-shell-module/add-random-dao-mei-dan.py
@@ -21,7 +21,7 @@ conn = mariadb.connect(
 cursor = conn.cursor()
 
 # Get the total number of users
-# 获得全部健康宝用户信息
+# 获得健康宝用户表
 cursor.execute("SELECT COUNT(*) FROM user")
 total_users = cursor.fetchone()[0]
 
@@ -29,13 +29,10 @@ total_users = cursor.fetchone()[0]
 # 随机选择倒霉蛋
 lucky_users = random.sample(range(1, total_users+1), args.count)
 
-# Update the "jackpot" column for the lucky users
-# 更新倒霉蛋的jackpot行，改为True，这下倒霉蛋就会被系统认定为核酸阳性并赋红码抓进方舱冲业绩
+# Update the "jackpot" column for the lucky users, and commit the changes to the database
+# 更新倒霉蛋的核酸为阳性，并提交数据库更改，这下倒霉蛋就会被系统认定为核酸阳性，一扫码就会变红码，即使不出门扫码的话jackpot为True也会有大白上门拿人抓进方舱冲业绩了
 for user_id in lucky_users:
     cursor.execute("UPDATE user SET jackpot=True WHERE id=?", (user_id,))
-
-# Commit the changes to the database
-# 提交数据库更改，这下用户一扫码就会变红码，即使不出门扫码的话jackpot为True也会有大白上门拿人了
 conn.commit()
 
 # Close the cursor and connection
