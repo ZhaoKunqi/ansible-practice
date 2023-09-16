cfe230f3dc45621b76fef61ae333d1d050085d27
Kunqi Zhao <110202952+ZhaoKunqi@users.noreply.github.com>
2023-09-16 23:33:07 +0800

    Create add-random-dao-mei-dan.py

---
+++ 03-use-shell-module/add-random-dao-mei-dan.py
@@ -0,0 +1,44 @@
+import mariadb
+import random
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--count', type=int, required=True)
+args = parser.parse_args()
+
+# Connect to the database
+# 连接到数据库
+conn = mariadb.connect(
+    user="admin",
+    password="zhong-nan-shan",
+    host="server185.healthcode.covid-apps.beijing.gov.cn",
+    port=3306,
+    database="hesuan"
+)
+
+# Create a cursor object
+# 创建一个游标对象来操作数据库
+cursor = conn.cursor()
+
+# Get the total number of users
+# 获得全部健康宝用户信息
+cursor.execute("SELECT COUNT(*) FROM user")
+total_users = cursor.fetchone()[0]
+
+# Randomly select the lucky users
+# 随机选择倒霉蛋
+lucky_users = random.sample(range(1, total_users+1), args.count)
+
+# Update the "jackpot" column for the lucky users
+# 更新倒霉蛋的jackpot行，改为True，这下倒霉蛋就会被系统认定为核酸阳性并赋红码抓进方舱冲业绩
+for user_id in lucky_users:
+    cursor.execute("UPDATE user SET jackpot=True WHERE id=?", (user_id,))
+
+# Commit the changes to the database
+# 提交数据库更改，这下用户一扫码就会变红码，即使不出门扫码的话jackpot为True也会有大白上门拿人了
+conn.commit()
+
+# Close the cursor and connection
+# 关闭游标和数据库链接，每日随机抓倒霉蛋进方舱任务完成
+cursor.close()
+conn.close()
